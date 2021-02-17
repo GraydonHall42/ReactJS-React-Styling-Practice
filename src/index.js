8032db4eb865a1b88339d1ea09553d95e2f38aa8
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-17 11:40:58 -0700

    Finished Challenge applied both CSS styling and dynamic inline styling to a greeting that changed depending on the time of day.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+var hour = new Date().getHours();
+console.log(hour);
+
+let greeting = "Hi!";
+let colorString = "Black";
+
+if (hour > 0 && hour < 12) {
+  greeting = "Good Morning";
+  colorString = "red";
+} else if (hour >= 12 && hour < 18) {
+  greeting = "Good Afternoon";
+  colorString = "green";
+} else {
+  greeting = "Good Evening";
+  colorString = "blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={{ color: colorString }}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
